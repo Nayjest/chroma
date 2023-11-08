fba8afd04c968daf7ec22586d9b5ff5ed175fb78
Nayjest <mail@vitaliy.in>
2023-11-08 20:47:41 +0100

    openai embedding impl. v2

--- chromadb/utils/embedding_functions.py
+++ chromadb/utils/embedding_functions.py
@@ -1,5 +1,4 @@
 import logging
-
 from chromadb.api.types import Documents, EmbeddingFunction, Embeddings
 from pathlib import Path
 import os
@@ -9,6 +8,7 @@ from typing import Any, Dict, List, cast
 import numpy as np
 import numpy.typing as npt
 import importlib
+import importlib.metadata
 import inspect
 import sys
 from typing import Optional
@@ -66,92 +66,64 @@ class Text2VecEmbeddingFunction(EmbeddingFunction):
         return self._model.encode(list(texts), convert_to_numpy=True).tolist()  # type: ignore # noqa E501
 
 
-class OpenAIEmbeddingFunction(EmbeddingFunction):
-    def __init__(
-        self,
-        api_key: Optional[str] = None,
-        model_name: str = "text-embedding-ada-002",
-        organization_id: Optional[str] = None,
-        api_base: Optional[str] = None,
-        api_type: Optional[str] = None,
-        api_version: Optional[str] = None,
-        deployment_id: Optional[str] = None,
-    ):
-        """
-        Initialize the OpenAIEmbeddingFunction.
-        Args:
-            api_key (str, optional): Your API key for the OpenAI API. If not
-                provided, it will raise an error to provide an OpenAI API key.
-            organization_id(str, optional): The OpenAI organization ID if applicable
-            model_name (str, optional): The name of the model to use for text
-                embeddings. Defaults to "text-embedding-ada-002".
-            api_base (str, optional): The base path for the API. If not provided,
-                it will use the base path for the OpenAI API. This can be used to
-                point to a different deployment, such as an Azure deployment.
-            api_type (str, optional): The type of the API deployment. This can be
-                used to specify a different deployment, such as 'azure'. If not
-                provided, it will use the default OpenAI deployment.
-            api_version (str, optional): The api version for the API. If not provided,
-                it will use the api version for the OpenAI API. This can be used to
-                point to a different deployment, such as an Azure deployment.
-            deployment_id (str, optional): Deployment ID for Azure OpenAI.
+try:
+    OPEN_AI_V1_USED = importlib.metadata.version("openai").startswith("1.")
+except importlib.metadata.PackageNotFoundError:
+    OPEN_AI_V1_USED = False
 
-        """
-        try:
-            import openai
-        except ImportError:
-            raise ValueError(
-                "The openai python package is not installed. Please install it with `pip install openai`"
-            )
 
-        if api_key is not None:
-            openai.api_key = api_key
-        # If the api key is still not set, raise an error
-        elif openai.api_key is None:
-            raise ValueError(
-                "Please provide an OpenAI API key. You can get one at https://platform.openai.com/account/api-keys"
-            )
+if OPEN_AI_V1_USED:
 
-        if api_base is not None:
-            openai.api_base = api_base
+    class OpenAIEmbeddingFunction(EmbeddingFunction):
+        def __init__(
+            self,
+            api_key: Optional[str] = None,
+            model_name: str = "text-embedding-ada-002",
+            api_base: Optional[str] = None,
+            api_type: Optional[str] = None,
+            api_version: Optional[str] = None,
+        ):
+            """
+            Initialize the OpenAIEmbeddingFunction.
+            Args:
+                api_key (str, optional): Your API key for the OpenAI API. If not
+                    provided, it will raise an error to provide an OpenAI API key.
+                model_name (str, optional): The name of the model to use for text
+                    embeddings. Defaults to "text-embedding-ada-002".
+                api_base (str, optional): The base path for the API. If not provided,
+                    it will use the base path for the OpenAI API. This can be used to
+                    point to a different deployment, such as an Azure deployment.
+                api_type (str, optional): The type of the API deployment. This can be
+                    used to specify a different deployment, such as 'azure'. If not
+                    provided, it will use the default OpenAI deployment.
+                api_version (str, optional): The api version for the API. If not provided,
+                    it will use the api version for the OpenAI API. This can be used to
+                    point to a different deployment, such as an Azure deployment.
 
-        if api_version is not None:
-            openai.api_version = api_version
-
-        self._api_type = api_type
-        if api_type is not None:
-            openai.api_type = api_type
-
-        if organization_id is not None:
-            openai.organization = organization_id
-
-        self._v1 = openai.__version__.startswith('1.')
-        if self._v1:
+            """
+            try:
+                import openai
+            except ImportError:
+                raise ValueError(
+                    "The openai python package is not installed. Please install it with `pip install openai`"
+                )
+            self._api_type = api_type
             if api_type == "azure":
                 self._client = openai.AzureOpenAI(
-                    api_key=api_key,
-                    api_version=api_version,
-                    azure_endpoint=api_base
+                    api_key=api_key, api_version=api_version, azure_endpoint=api_base
                 ).embeddings
             else:
                 self._client = openai.OpenAI(
-                    api_key=api_key,
-                    base_url=api_base
+                    api_key=api_key, base_url=api_base
                 ).embeddings
-        else:
-            self._client = openai.Embedding
-        self._model_name = model_name
-        self._deployment_id = deployment_id
+            self._model_name = model_name
 
-    def __call__(self, texts: Documents) -> Embeddings:
-        # replace newlines, which can negatively affect performance.
-        texts = [t.replace("\n", " ") for t in texts]
+        def __call__(self, texts: Documents) -> Embeddings:
+            # replace newlines, which can negatively affect performance.
+            texts = [t.replace("\n", " ") for t in texts]
 
-        # Call the OpenAI Embedding API
-        if self._v1:
             embeddings = self._client.create(
-                input=texts,
-                model=self._deployment_id or self._model_name
+                input=texts, model=self._deployment_id or self._model_name
             ).data
 
             # Sort resulting embeddings by index
@@ -159,14 +131,84 @@ class OpenAIEmbeddingFunction(EmbeddingFunction):
 
             # Return just the embeddings
             return [result.embedding for result in sorted_embeddings]
-        else:
+
+else:
+
+    class OpenAIEmbeddingFunction(EmbeddingFunction):
+        def __init__(
+            self,
+            api_key: Optional[str] = None,
+            model_name: str = "text-embedding-ada-002",
+            organization_id: Optional[str] = None,
+            api_base: Optional[str] = None,
+            api_type: Optional[str] = None,
+            api_version: Optional[str] = None,
+            deployment_id: Optional[str] = None,
+        ):
+            """
+            Initialize the OpenAIEmbeddingFunction.
+            Args:
+                api_key (str, optional): Your API key for the OpenAI API. If not
+                    provided, it will raise an error to provide an OpenAI API key.
+                organization_id(str, optional): The OpenAI organization ID if applicable
+                model_name (str, optional): The name of the model to use for text
+                    embeddings. Defaults to "text-embedding-ada-002".
+                api_base (str, optional): The base path for the API. If not provided,
+                    it will use the base path for the OpenAI API. This can be used to
+                    point to a different deployment, such as an Azure deployment.
+                api_type (str, optional): The type of the API deployment. This can be
+                    used to specify a different deployment, such as 'azure'. If not
+                    provided, it will use the default OpenAI deployment.
+                api_version (str, optional): The api version for the API. If not provided,
+                    it will use the api version for the OpenAI API. This can be used to
+                    point to a different deployment, such as an Azure deployment.
+                deployment_id (str, optional): Deployment ID for Azure OpenAI.
+
+            """
+            try:
+                import openai
+            except ImportError:
+                raise ValueError(
+                    "The openai python package is not installed. Please install it with `pip install openai`"
+                )
+
+            if api_key is not None:
+                openai.api_key = api_key
+            # If the api key is still not set, raise an error
+            elif openai.api_key is None:
+                raise ValueError(
+                    "Please provide an OpenAI API key. You can get one at https://platform.openai.com/account/api-keys"
+                )
+
+            if api_base is not None:
+                openai.api_base = api_base
+
+            if api_version is not None:
+                openai.api_version = api_version
+
+            if api_type is not None:
+                openai.api_type = api_type
+
+            if organization_id is not None:
+                openai.organization = organization_id
+
+            self._client = openai.Embedding
+            self._model_name = model_name
+            self._deployment_id = deployment_id
+
+        def __call__(self, texts: Documents) -> Embeddings:
+            # replace newlines, which can negatively affect performance.
+            texts = [t.replace("\n", " ") for t in texts]
+
+            # Call the OpenAI Embedding API
             if self._api_type == "azure":
                 embeddings = self._client.create(
-                    input=texts,
-                    engine=self._deployment_id or self._model_name
+                    input=texts, engine=self._deployment_id or self._model_name
                 )["data"]
             else:
-                embeddings = self._client.create(input=texts, model=self._model_name)["data"]
+                embeddings = self._client.create(
+                    input=texts, model=self._model_name
+                )["data"]
 
             # Sort resulting embeddings by index
             sorted_embeddings = sorted(embeddings, key=lambda e: e["index"])  # type: ignore
